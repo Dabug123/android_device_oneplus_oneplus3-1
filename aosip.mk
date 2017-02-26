958021e8157f9e675d2e931a41a0152d95f095d0
Paul Clark <clarkbrunswick55@gmail.com>
2017-02-26 12:12:39 -0500

    new base

--- aosip.mk
+++ aosip.mk
@@ -1,5 +1,4 @@
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,9 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common DU stuff.
-$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosip/common.mk)
 
-PRODUCT_NAME := du_oneplus3
+PRODUCT_NAME := aosip_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
